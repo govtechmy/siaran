55a424a957b66958afa0c5dafbcaa88ea9b963df
Wan Aqim <wanaqim@gmail.com>
2024-09-10 11:59:07 +0800

    create Agency schema

---
+++ siaran-gov-back/src/collections/PressRelease.ts
@@ -0,0 +1,57 @@
+import { CollectionConfig } from 'payload/types';
+
+const PressRelease: CollectionConfig = {
+  slug: 'press-releases',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'snippet',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+    },
+    {
+      name: 'datetime',
+      type: 'date',
+      required: true,
+    },
+    {
+      name: 'attachments',
+      type: 'array',
+      fields: [
+        {
+          name: 'fileUrl',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'filename',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'filesize',
+          type: 'number',
+          required: true,
+        },
+      ],
+      required: false,
+    },
+    {
+      name: 'relatedAgency',
+      type: 'relationship',
+      relationTo: 'agencies',
+      required: true,
+    },
+  ],
+};
+
+export default PressRelease;
