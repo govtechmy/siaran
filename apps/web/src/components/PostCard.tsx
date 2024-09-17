48e446020a2462620721a6abddba1126a761f7f6
Wan Aqim <wanaqim@gmail.com>
2024-09-17 16:02:04 +0800

    truncate content

--- apps/web/src/components/PostCard.tsx
+++ apps/web/src/components/PostCard.tsx
@@ -1,4 +1,3 @@
-// components/PostCard.tsx
 import React from 'react';
 import { Post } from '@/app/types/types';
 
@@ -6,12 +5,20 @@ interface PostCardProps {
   post: Post;
 }
 
+const truncateContent = (content: string, maxWords: number): string => {
+  const words = content.split(' '); 
+  if (words.length > maxWords) {
+    return words.slice(0, maxWords).join(' ') + '...'; 
+  }
+  return content;
+};
+
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
     <div className="post-card">
       <h3>{post.title}</h3>
-      <p>{new Date(post.datetime).toLocaleDateString()}</p>
-      <p>{post.summary}</p>
+      <p>{new Date(post.date_published).toLocaleDateString()}</p>
+      <p>{truncateContent(post.content.plain, 20)}</p>
     </div>
   );
 };
