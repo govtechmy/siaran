431045642d534aa33cfe767ecffc098c10c07a74
Wan Aqim <wanaqim@gmail.com>
2024-09-17 16:02:24 +0800

    build pagination logic

--- apps/web/src/components/PostsBox.tsx
+++ apps/web/src/components/PostsBox.tsx
@@ -1,18 +1,43 @@
-// components/PostsBox.tsx
+'use client';
+
 import React from 'react';
-import { Post } from '@/app/types/types';
 import PostCard from './PostCard';
+import Pagination from '@/components/ui/pagination';
+import { PaginatedResponse, Post } from '@/app/types/types';
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 interface PostsBoxProps {
-  posts: Post[];
+  posts: PaginatedResponse<Post>; 
 }
 
 const PostsBox: React.FC<PostsBoxProps> = ({ posts }) => {
+  const { docs: data, totalPages, page } = posts;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= totalPages) {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set('page', page.toString());
+      router.push(`${window.location.pathname}?${params.toString()}`);
+    }
+  };
+
   return (
-    <div className="posts-box">
-      {posts.map((post) => (
-        <PostCard key={post.id} post={post} />
-      ))}
+    <div className="flex flex-col justify-center gap-4">
+      <div className="flex flex-col gap-6">
+        {data.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
+      </div>
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={page}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      )}
     </div>
   );
 };
