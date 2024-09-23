5717687255c2d15150f890915b91ede8a4204c23
Jesse Soo <jesse.soo.my@gmail.com>
2024-09-23 18:50:17 +0800

    setup: trpc

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,16 @@
 import { serve } from "@hono/node-server";
+import { trpcServer } from "@hono/trpc-server";
+import { appRouter } from "@repo/api/trpc-router";
 import { Hono } from "hono";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("OK");
-});
+app.use(
+  "/api/*",
+  trpcServer({
+    router: appRouter,
+  })
+);
 
 const port = parseInt(process.env.PORT || "8080");
 console.log(`Server is running on port ${port}`);
