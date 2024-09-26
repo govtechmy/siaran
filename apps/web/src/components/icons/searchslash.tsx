a09959943e9e38776f3e4796b8010816fb6e45ab
Wan Aqim <wanaqim@gmail.com>
2024-09-26 14:09:54 +0800

    add icons

---
+++ apps/web/src/components/icons/searchslash.tsx
@@ -0,0 +1,28 @@
+export default function SearchSlash({
+    className = 'stroke-[#18181B] dark:stroke-[#FFFFFF]',
+    ...props
+  }) {
+    return (
+<svg width="23" height="28" viewBox="0 0 23 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_17624_12499)">
+<rect x="3" y="2" width="17" height="22" rx="6" fill="white" shape-rendering="crispEdges"/>
+<rect x="3.5" y="2.5" width="16" height="21" rx="5.5" stroke="#D4D4D8" shape-rendering="crispEdges"/>
+<path d="M13.1804 7.86364L10.3679 18.3125H9.23011L12.0426 7.86364H13.1804Z" fill="#71717A"/>
+</g>
+<defs>
+<filter id="filter0_d_17624_12499" x="0" y="0" width="23" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="1"/>
+<feGaussianBlur stdDeviation="1.5"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.07 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_17624_12499"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_17624_12499" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+    );
+  }
+  
