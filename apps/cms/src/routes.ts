7702a6600a09cd8e2b47551e159b7049a9971b93
Wan Aqim <wanaqim@gmail.com>
2024-09-27 16:06:07 +0800

    add /search endpoint

--- apps/cms/src/routes.ts
+++ apps/cms/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getPressReleases, getPressReleasesByAgency,  } from './controllers/api/PressReleases';
+import { Search } from './controllers/api/Search';
 
 const router = Router();
 
 router.get('/press-releases', getPressReleases);
 // router.post('/press-release/create', createPressRelease);
 router.get('/press-releases/by-agency', getPressReleasesByAgency);
+router.get('/search', Search);
 
 export default router;
