d935fe44ee63d5f583033c013775ab0efb65f216
Wan Aqim <wanaqim@gmail.com>
2024-10-02 15:33:21 +0800

    more icons

--- apps/web/src/components/icons/searchslash.tsx
+++ apps/web/src/components/icons/searchslash.tsx
@@ -5,13 +5,13 @@ export default function SearchSlash({
     return (
 <svg width="23" height="28" viewBox="0 0 23 28" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g filter="url(#filter0_d_17624_12499)">
-<rect x="3" y="2" width="17" height="22" rx="6" fill="white" shape-rendering="crispEdges"/>
-<rect x="3.5" y="2.5" width="16" height="21" rx="5.5" stroke="#D4D4D8" shape-rendering="crispEdges"/>
+<rect x="3" y="2" width="17" height="22" rx="6" fill="white" shapeRendering="crispEdges"/>
+<rect x="3.5" y="2.5" width="16" height="21" rx="5.5" stroke="#D4D4D8" shapeRendering="crispEdges"/>
 <path d="M13.1804 7.86364L10.3679 18.3125H9.23011L12.0426 7.86364H13.1804Z" fill="#71717A"/>
 </g>
 <defs>
-<filter id="filter0_d_17624_12499" x="0" y="0" width="23" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<filter id="filter0_d_17624_12499" x="0" y="0" width="23" height="28" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+<feFlood floodOpacity="0" result="BackgroundImageFix"/>
 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
 <feOffset dy="1"/>
 <feGaussianBlur stdDeviation="1.5"/>
