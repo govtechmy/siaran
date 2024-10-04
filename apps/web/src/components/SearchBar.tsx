ba7c43d713e648e1731217ad8a038f605978608b
Wan Aqim <wanaqim@gmail.com>
2024-10-04 09:46:35 +0800

    add agencydropdown component

--- apps/web/src/components/SearchBar.tsx
+++ apps/web/src/components/SearchBar.tsx
@@ -1,33 +1,43 @@
 'use client'
-import React, {useState} from "react";
+import React from "react";
 import GovBadge from "./icons/govbadge";
 import Section from "./Section";
 import InputSearchBar from "./InputSearchBar";
+import AgencyDropDown from "./AgencyDropdown";
+import type { Agency } from '@/app/types/types';
 
-export default function SearchBar() {
-    const [searchTerm, setSearchTerm] = useState('');
+interface SearchBarProps {
+  agencies: Agency[];
+}
 
+export default function SearchBar({ agencies }: SearchBarProps) {
     return (
-        <div
+      <div
         className="flex flex-col items-center py-[3rem] px-[1.5rem] min-h-[15rem] w-full bg-[radial-gradient(117.1%_158.96%_at_50%_-58.96%,#FFD0A9_0%,#FFF6ED_100%)]"
-        > 
+      >
         <Section>
-            <div className="flex items-center w-full relative">
+          <div className="flex items-center w-full relative">
             <h1 className="text-siaran-text_hero text-xxl h-[2rem] mx-auto text-center">
-                One Stop Government Press Releases & Announcements
+              One Stop Government Press Releases & Announcements
             </h1>
             <div className="absolute right-0 pt-7">
-                <GovBadge className="ml-auto"/>
+              <GovBadge className="ml-auto" />
             </div>
-            </div>
+          </div>
         </Section>
         <Section>
-            <div className=" flex flex-col items-center">
-            <div className="flex flex-col items-center w-[600px] h-[88px] pt-7">
-                <InputSearchBar/>
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center w-[600px] h-[88px] pt-7">
+                <InputSearchBar />
+              </div>
             </div>
+            <div className="w-[503px] h-[32px] flex mt-1 items-center gap-3">
+              <span className="text-sm text-gray-500">Filter by:</span>
+              <AgencyDropDown agencies={agencies} />
             </div>
+          </div>
         </Section>
-        </div>
+      </div>
     );
-}
+  }
