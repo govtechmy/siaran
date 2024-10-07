19428f2a16bb31f76ae3ab76b5d78d2a878bf3a1
Wan Aqim <wanaqim@gmail.com>
2024-10-07 09:53:16 +0800

    create more icons

--- apps/web/src/components/icons/chevron-right.tsx
+++ apps/web/src/components/icons/chevron-right.tsx
@@ -4,7 +4,7 @@ export default function ChevronRight({
   }) {
     return (
 <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7 4.8335L11.1667 9.00016L7 13.1668" stroke="#71717A" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M7 4.8335L11.1667 9.00016L7 13.1668" stroke="#71717A" stroke-width="1.25" strokeLinecap="round" />
 </svg>
     );
   }
--- apps/web/src/components/icons/cross-x.tsx
+++ apps/web/src/components/icons/cross-x.tsx
@@ -5,7 +5,7 @@ export default function CrossX({
   }) {
     return (
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.32 4.67993L4.67999 13.3199M4.67999 4.67993L13.32 13.3199" stroke="#71717A" stroke-width="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M13.32 4.67993L4.67999 13.3199M4.67999 4.67993L13.32 13.3199" stroke="#71717A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
     );
   }
