7342d964bf5b3a8ff6c8055f1dfbb0c4c8887a67
Wan Aqim <wanaqim@gmail.com>
2024-10-09 11:29:29 +0800

    refine with time

--- apps/cms/src/controllers/api/press-releases.ts
+++ apps/cms/src/controllers/api/press-releases.ts
@@ -2,52 +2,61 @@ import payload from "payload";
 
 export const list = async (req, res) => {
   try {
-    const { page = 1, limit = 10, agency, mediaType, fromDate, toDate } = req.query;
-    let where = {
-
-    };
+    const {
+      page = 1,
+      limit = 12,
+      agency,
+      mediaType,
+      fromDate,
+      toDate,
+    } = req.query;
+    let where = {};
 
     if (agency) {
       where["relatedAgency"] = {
-        equals: agency, 
+        equals: agency,
       };
     }
 
     if (mediaType && mediaType !== "All") {
       if (mediaType === "Media Release") {
         where["type"] = {
-          equals: "kenyataan_media", 
+          equals: "kenyataan_media",
         };
       } else if (mediaType === "Speech Collection") {
         where["type"] = {
-          equals: "ucapan", 
+          equals: "ucapan",
         };
       }
     }
 
     if (fromDate) {
       const from = new Date(fromDate);
-      console.log('From Date:', from);
+      from.setHours(0, 0, 0, 0);
 
       if (!isNaN(from.getTime())) {
-        where['date_published'] = { greater_than_equal: from };
+        where["date_published"] = { greater_than_equal: from.toISOString() };
       } else {
-        return res.status(400).json({ error: 'Invalid fromDate format' });
+        return res.status(400).json({ error: "Invalid fromDate format" });
       }
     }
 
     if (toDate) {
       const to = new Date(toDate);
-      console.log('To Date:', to);
+      to.setHours(23, 59, 59, 999);
 
       if (!isNaN(to.getTime())) {
-        where['date_published'] = where['date_published']
-          ? { ...where['date_published'], less_than_equal: to }
-          : { less_than_equal: to };
+        where["date_published"] = where["date_published"]
+          ? {
+              ...where["date_published"],
+              less_than_equal: to.toISOString(),
+            }
+          : { less_than_equal: to.toISOString() };
       } else {
-        return res.status(400).json({ error: 'Invalid toDate format' });
+        return res.status(400).json({ error: "Invalid toDate format" });
       }
     }
+
 
     const pressReleases = await payload.find({
       collection: "press-releases",
@@ -65,4 +74,3 @@ export const list = async (req, res) => {
       .json({ error: "An error occurred while fetching press releases." });
   }
 };
-
