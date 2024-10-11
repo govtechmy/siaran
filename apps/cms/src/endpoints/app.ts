d32e50ea9c4f422ff9c7734b522581e65a55e60c
Wan Aqim <wanaqim@gmail.com>
2024-10-11 11:18:11 +0800

    import pressrelease endpoints

--- apps/cms/src/endpoints/app.ts
+++ apps/cms/src/endpoints/app.ts
@@ -1,4 +1,4 @@
 import { endpoints as searchEndpoints } from "./search";
-
+import { endpoints as pressReleaseEndpoints } from "./press-releases";
 // For non collection-specific endpoints only
 export default [...searchEndpoints];
